b5707152b0b3aaf79a7c9c1d1783f0d1bc3fe388
FDC-junery <48433196+FDC-junery@users.noreply.github.com>
2019-03-12 11:31:49 +0800

    Login Test Cases
    - Added modification on the checking after login for all 3 test cases

--- src/com/company/Test_SeleniumTest.java
+++ src/com/company/Test_SeleniumTest.java
@@ -1,95 +1,123 @@
 package com.company;
 
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.internal.WebElementToJsonConverter;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.EmptyStackException;
 
 
 public class Test_SeleniumTest {
+    private WebDriver webD = null;
+
+    @Before
+    public void Setup()
+    {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Documents\\chromedriver.exe");
+
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("incognito", "disable-infobars", "start-maximized");
+
+        webD = new ChromeDriver(option);
+        webD.manage().window().maximize();
+
+    }
+
+    @After
+    public void Exit()
+    {
+
+        try
+        {
+           webD.quit();
+        }
+        catch(NullPointerException n) {}
+
+    }
+
     @Test
     public void LoginSuccess() throws InterruptedException
     {
-        String home = System.getProperty(("user.home"));
 
-        System.setProperty("webdriver.chrome.driver", "/Users/lesterpadul/Desktop/chromedriver");
-        WebDriver webD = new ChromeDriver();
         webD.get("https://english.fdc-inc.com/");
 
-
         WebElement txtSearch;
-        WebElement LoginBtn;
-        LoginBtn = webD.findElement(By.xpath("//a[contains(@href, '/login')]"));
+
+        WebElement LoginBtn = webD.findElement(By.xpath("//a[contains(@href, '/login')]"));
         LoginBtn.click();
+
         txtSearch = webD.findElement(By.id("UserEmail"));
         txtSearch.sendKeys("jengo@gmail.com");
+
         txtSearch = webD.findElement(By.id("UserPassword"));
         txtSearch.sendKeys("admin123");
+
         txtSearch = webD.findElement(By.xpath("//button[@type='submit']"));
-        txtSearch.submit();
+        txtSearch.click();
 
         try
         {
             Thread.sleep(10000);
         }catch (InterruptedException e){}
 
-
-        if(webD.findElement(By.xpath("//button[@type='submit']")).isDisplayed())
+        String currentUrl = webD.getCurrentUrl();
+        System.out.print("URL After hitting login button: " + currentUrl);
+        if(currentUrl.contains("login"))
         {
-            webD.quit();
-            Assert.fail("Failed to Login after 10 seconds");
+            Assert.fail("Failed to Login after 10 seconds, Please check your login credential");
         }
 
         webD.get("https://english.fdc-inc.com/logout");
-        webD.quit();
 
     }
 
     @Test
     public void LoginNoInput()
     {
-        String home = System.getProperty(("user.home"));
 
-        System.setProperty("webdriver.chrome.driver", "/Users/lesterpadul/Desktop/chromedriver");
-        WebDriver webD = new ChromeDriver();
         webD.get("https://english.fdc-inc.com/");
-
 
         WebElement txtSearch;
         WebElement LoginBtn;
+
         LoginBtn = webD.findElement(By.xpath("//a[contains(@href, '/login')]"));
         LoginBtn.click();
+
         txtSearch = webD.findElement(By.id("UserEmail"));
         txtSearch.sendKeys("");
+
         txtSearch = webD.findElement(By.id("UserPassword"));
         txtSearch.sendKeys("");
+
         txtSearch = webD.findElement(By.xpath("//button[@type='submit']"));
-        txtSearch.submit();
+        txtSearch.click();
 
-        if(webD.findElement(By.xpath("//div[@id='authMessage']")).isDisplayed())
+        try
         {
-            webD.quit();
-            Assert.fail("No Input Values for UserEmail and Password.");
+            Thread.sleep(10000);
+        }catch (InterruptedException e){}
+
+        String currentUrl = webD.getCurrentUrl();
+        System.out.print("URL After hitting login button: " + currentUrl);
+
+        if(currentUrl.contains("login"))
+        {
+            Assert.fail("Failed to Login after 10 seconds, Please check your login credential");
         }
-
-
 
     }
 
     @Test
     public void LoginIncorrectPassword()
     {
-        String home = System.getProperty(("user.home"));
 
-        System.setProperty("webdriver.chrome.driver", "/Users/lesterpadul/Desktop/chromedriver");
-        WebDriver webD = new ChromeDriver();
         webD.get("https://english.fdc-inc.com/");
 
 
@@ -97,20 +125,23 @@ public class Test_SeleniumTest {
         WebElement LoginBtn;
         LoginBtn = webD.findElement(By.xpath("//a[contains(@href, '/login')]"));
         LoginBtn.click();
+
         txtSearch = webD.findElement(By.id("UserEmail"));
         txtSearch.sendKeys("jengo@gmail.com");
+
         txtSearch = webD.findElement(By.id("UserPassword"));
         txtSearch.sendKeys("test");
+
         txtSearch = webD.findElement(By.xpath("//button[@type='submit']"));
-        txtSearch.submit();
+        txtSearch.click();
 
-        if(webD.findElement(By.xpath("//div[@id='authMessage']")).isDisplayed()) {
-            webD.quit();
-            System.out.println(webD.findElement(By.xpath("//div[@id='authMessage']")).getText());
-            Assert.fail("No Input Values for UserEmail and Password");
+        String currentUrl = webD.getCurrentUrl();
+        System.out.print("URL After hitting login button: " + currentUrl);
+
+        if(currentUrl.contains("login"))
+        {
+            Assert.fail("Failed to Login after 10 seconds, Please check your login credential");
         }
-
-
 
     }
 
